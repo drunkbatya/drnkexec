5410c6022973d991fcf4a10289a0d51ecfb76d5d
DrunkBatya <drunkbatya.js@gmail.com>
2026-03-01 23:23:32 +0300

    Fix ping again

--- server/backend/src/internal/pinger/pinger.go
+++ server/backend/src/internal/pinger/pinger.go
@@ -43,6 +43,7 @@ func (p *probingChecker) Ping(ctx context.Context, host *model.HostConfig) (stri
 	pinger.Count = defaultPingCount
 	pinger.Timeout = defaultPingTimeout
 	pinger.Interval = defaultPingTimeout
+	pinger.SetPrivileged(true)
 
 	runErr := make(chan error, 1)
 	go func() {
